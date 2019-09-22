1bf76fc3ce88c3a5e31eda7e053ccf0f74d04c62
Cody <ChaosZero112@users.noreply.github.com>
2019-09-22 01:19:42 -0400

    Update docker.sh
    Fixed remaining "SSH_PATH" reference(s).

--- docker.sh
+++ docker.sh
@@ -23,15 +23,15 @@ echo "" >> /cron-script
 if [ -n "${REMOTE_SSH_KEY}" ] && [ -n "${REMOTE_SSH_PORT}" ] && [ -n "${REMOTE_SSH_USER}" ] && [ -n "${REMOTE_SSH_HOST}" ] && [ -n "${REMOTE_DB_PATH}" ]; then
 	mkdir -p /mnt/S1
 	V_REMOTE_DB_PATH="/mnt/S1"
-	echo -e "echo \x22[\`date\`] Mounting sshfs for server 1...\x22" >> /cron-script
-	echo -e "sshfs -o allow_other,cache=no,no_readahead,noauto_cache,StrictHostKeyChecking=no,IdentityFile=\x22${REMOTE_SSH_KEY}\x22 -p ${REMOTE_SSH_PORT} ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST}:\x22${REMOTE_SSH_PATH}\x22 /mnt/S1" >> /cron-script
+	echo -e "echo \x22[\`date\`] Mounting sshfs for ${REMOTE_SSH_HOST}...\x22" >> /cron-script
+	echo -e "sshfs -o allow_other,cache=no,no_readahead,noauto_cache,StrictHostKeyChecking=no,IdentityFile=\x22${REMOTE_SSH_KEY}\x22 -p ${REMOTE_SSH_PORT} ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST}:\x22${REMOTE_DB_PATH}\x22 /mnt/S1" >> /cron-script
 fi
 if [[ "${LOCAL_PATH_IS_SSH}" == [Tt]rue ]] || [[ "${LOCAL_PATH_IS_SSH}" == [Yy]es ]]; then
 	if [ -n "${LOCAL_SSH_KEY}" ] && [ -n "${LOCAL_SSH_PORT}" ] && [ -n "${LOCAL_SSH_USER}" ] && [ -n "${LOCAL_SSH_HOST}" ] && [ -n "${LOCAL_DB_PATH}" ]; then
 		mkdir -p /mnt/S2
 		V_LOCAL_DB_PATH="/mnt/S2"
-		echo -e "echo \x22[\`date\`] Mounting sshfs for server 2...\x22" >> /cron-script
-		echo -e "sshfs -o allow_other,cache=no,no_readahead,noauto_cache,StrictHostKeyChecking=no,IdentityFile=\x22${LOCAL_SSH_KEY}\x22 -p ${LOCAL_SSH_PORT} ${LOCAL_SSH_USER}@${LOCAL_SSH_HOST}:\x22${LOCAL_SSH_PATH}\x22 /mnt/S2" >> /cron-script
+		echo -e "echo \x22[\`date\`] Mounting sshfs for ${LOCAL_SSH_HOST}...\x22" >> /cron-script
+		echo -e "sshfs -o allow_other,cache=no,no_readahead,noauto_cache,StrictHostKeyChecking=no,IdentityFile=\x22${LOCAL_SSH_KEY}\x22 -p ${LOCAL_SSH_PORT} ${LOCAL_SSH_USER}@${LOCAL_SSH_HOST}:\x22${LOCAL_DB_PATH}\x22 /mnt/S2" >> /cron-script
 	fi
 fi
 echo -e "/plex-db-sync --dry-run \x22${V_DRYRUN}\x22 --backup \x22${V_BACKUP}\x22 --debug \x22${V_DEBUG}\x22 --tmp-folder \x22${V_TMPFOLDER}\x22 --plex-db-1 \x22${V_REMOTE_DB_PATH}/com.plexapp.plugins.library.db\x22 --plex-start-1 \x22${REMOTE_START}\x22 --plex-stop-1 \x22${REMOTE_STOP}\x22 --plex-db-2 \x22${V_LOCAL_DB_PATH}/com.plexapp.plugins.library.db\x22 --plex-start-2 \x22${LOCAL_START}\x22 --plex-stop-2 \x22${LOCAL_STOP}\x22 --ignore-accounts \x22${IGNOREACCOUNTS}\x22" >> /cron-script

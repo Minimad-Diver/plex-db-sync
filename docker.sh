1ddda8a57ea593cc5d5244b0458f7f785ce57250
Minimad-Diver <35314158+Minimad-Diver@users.noreply.github.com>
2022-06-16 18:42:06 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -8,45 +8,37 @@ function echoDN {
 	echo -n "[`date`] ${@}"
 }
 
-V_REMOTE_DB_PATH=""; if [ -n "${REMOTE_DB_PATH}" ]; then V_REMOTE_DB_PATH="${REMOTE_DB_PATH}"; fi
-V_LOCAL_DB_PATH=""; if [ -n "${LOCAL_DB_PATH}" ]; then V_LOCAL_DB_PATH="${LOCAL_DB_PATH}"; fi
+V_S1_DB_PATH=""; if [ -n "${S1_DB_PATH}" ]; then V_S1_DB_PATH="${S1_DB_PATH}"; fi
+V_S2_DB_PATH=""; if [ -n "${S2_DB_PATH}" ]; then V_S2_DB_PATH="${S2_DB_PATH}"; fi
 V_BACKUP="false"; if [ -n "${BACKUP}" ]; then V_BACKUP="${BACKUP}"; fi
 V_DEBUG="false"; if [ -n "${DEBUG}" ]; then V_DEBUG="${DEBUG}"; fi
 V_DRYRUN="false"; if [ -n "${DRYRUN}" ]; then V_DRYRUN="${DRYRUN}"; fi
 V_TMPFOLDER="/tmp/plex-db-sync"; if [ -n "${TMPFOLDER}" ]; then V_TMPFOLDER="${TMPFOLDER}"; fi
 V_CRON="0 4 * * *"; if [ -n "${CRON}" ]; then V_CRON="${CRON}"; fi
-V_REMOTE_STOP="docker stop plex"; if [ -n "${REMOTE_STOP}" ]; then V_REMOTE_STOP="ssh -oStrictHostKeyChecking=no -i ${REMOTE_SSH_KEY} -p ${REMOTE_SSH_PORT} ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST} '${REMOTE_STOP}'"; fi
-V_REMOTE_START="docker start plex"; if [ -n "${REMOTE_START}" ]; then V_REMOTE_START="ssh -oStrictHostKeyChecking=no -i ${REMOTE_SSH_KEY} -p ${REMOTE_SSH_PORT} ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST} '${REMOTE_START}'"; fi
-
 
 echo "#!/bin/bash" > /cron-script
 echo "" >> /cron-script
 
 # Set up cron script
-if [ -n "${REMOTE_SSH_KEY}" ] && [ -n "${REMOTE_SSH_PORT}" ] && [ -n "${REMOTE_SSH_USER}" ] && [ -n "${REMOTE_SSH_HOST}" ] && [ -n "${REMOTE_DB_PATH}" ]; then
+if [ -n "${S1_SSH_KEY}" ] && [ -n "${S1_SSH_PORT}" ] && [ -n "${S1_SSH_USER}" ] && [ -n "${S1_SSH_HOST}" ] && [ -n "${S1_SSH_PATH}" ]; then
 	mkdir -p /mnt/S1
-	V_REMOTE_DB_PATH="/mnt/S1"
-	echo -e "echo \x22[\`date\`] Mounting sshfs for ${REMOTE_SSH_HOST}...\x22" >> /cron-script
-	echo -e "sshfs -o allow_other,cache=no,no_readahead,noauto_cache,StrictHostKeyChecking=no,IdentityFile=\x22${REMOTE_SSH_KEY}\x22 -p ${REMOTE_SSH_PORT} ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST}:\x22${REMOTE_DB_PATH}\x22 /mnt/S1" >> /cron-script
+	V_S1_DB_PATH="/mnt/S1"
+	echo -e "echo \x22[\`date\`] Mounting sshfs for server 1...\x22" >> /cron-script
+	echo -e "sshfs -o allow_other,cache=no,no_readahead,noauto_cache,StrictHostKeyChecking=no,IdentityFile=\x22${S1_SSH_KEY}\x22 -p ${S1_SSH_PORT} ${S1_SSH_USER}@${S1_SSH_HOST}:\x22${S1_SSH_PATH}\x22 /mnt/S1" >> /cron-script
 fi
-if [[ "${LOCAL_PATH_IS_SSH}" == [Tt]rue ]] || [[ "${LOCAL_PATH_IS_SSH}" == [Yy]es ]]; then
-	if [ -n "${LOCAL_SSH_KEY}" ] && [ -n "${LOCAL_SSH_PORT}" ] && [ -n "${LOCAL_SSH_USER}" ] && [ -n "${LOCAL_SSH_HOST}" ] && [ -n "${LOCAL_DB_PATH}" ]; then
-		mkdir -p /mnt/S2
-		V_LOCAL_DB_PATH="/mnt/S2"
-		echo -e "echo \x22[\`date\`] Mounting sshfs for ${LOCAL_SSH_HOST}...\x22" >> /cron-script
-		echo -e "sshfs -o allow_other,cache=no,no_readahead,noauto_cache,StrictHostKeyChecking=no,IdentityFile=\x22${LOCAL_SSH_KEY}\x22 -p ${LOCAL_SSH_PORT} ${LOCAL_SSH_USER}@${LOCAL_SSH_HOST}:\x22${LOCAL_DB_PATH}\x22 /mnt/S2" >> /cron-script
-	fi
+if [ -n "${S2_SSH_KEY}" ] && [ -n "${S2_SSH_PORT}" ] && [ -n "${S2_SSH_USER}" ] && [ -n "${S2_SSH_HOST}" ] && [ -n "${S2_SSH_PATH}" ]; then
+	mkdir -p /mnt/S2
+	V_S2_DB_PATH="/mnt/S2"
+	echo -e "echo \x22[\`date\`] Mounting sshfs for server 2...\x22" >> /cron-script
+	echo -e "sshfs -o allow_other,cache=no,no_readahead,noauto_cache,StrictHostKeyChecking=no,IdentityFile=\x22${S2_SSH_KEY}\x22 -p ${S2_SSH_PORT} ${S2_SSH_USER}@${S2_SSH_HOST}:\x22${S2_SSH_PATH}\x22 /mnt/S2" >> /cron-script
 fi
-echo -e "/plex-db-sync --dry-run \x22${V_DRYRUN}\x22 --backup \x22${V_BACKUP}\x22 --debug \x22${V_DEBUG}\x22 --tmp-folder \x22${V_TMPFOLDER}\x22 --plex-db-1 \x22${V_REMOTE_DB_PATH}/com.plexapp.plugins.library.db\x22 --plex-start-1 \x22${V_REMOTE_START}\x22 --plex-stop-1 \x22${V_REMOTE_STOP}\x22 --plex-db-2 \x22${V_LOCAL_DB_PATH}/com.plexapp.plugins.library.db\x22 --plex-start-2 \x22${LOCAL_START}\x22 --plex-stop-2 \x22${LOCAL_STOP}\x22 --ignore-accounts \x22${IGNOREACCOUNTS}\x22" >> /cron-script
-if [ -n "${REMOTE_SSH_KEY}" ] && [ -n "${REMOTE_SSH_PORT}" ] && [ -n "${REMOTE_SSH_USER}" ] && [ -n "${REMOTE_SSH_HOST}" ] && [ -n "${REMOTE_DB_PATH}" ]; then
+echo -e "/plex-db-sync --dry-run \x22${V_DRYRUN}\x22 --backup \x22${V_BACKUP}\x22 --debug \x22${V_DEBUG}\x22 --tmp-folder \x22${V_TMPFOLDER}\x22 --plex-db-1 \x22${V_S1_DB_PATH}/com.plexapp.plugins.library.db\x22 --plex-start-1 \x22${S1_START}\x22 --plex-stop-1 \x22${S1_STOP}\x22 --plex-db-2 \x22${V_S2_DB_PATH}/com.plexapp.plugins.library.db\x22 --plex-start-2 \x22${S2_START}\x22 --plex-stop-2 \x22${S2_STOP}\x22 --ignore-accounts \x22${IGNOREACCOUNTS}\x22" >> /cron-script
+if [ -n "${S1_SSH_KEY}" ] && [ -n "${S1_SSH_PORT}" ] && [ -n "${S1_SSH_USER}" ] && [ -n "${S1_SSH_HOST}" ] && [ -n "${S1_SSH_PATH}" ]; then
 	echo "umount /mnt/S1" >> /cron-script
 fi
-if [[ "${LOCAL_PATH_IS_SSH}" == [Tt]rue ]] || [[ "${LOCAL_PATH_IS_SSH}" == [Yy]es ]]; then
-	if [ -n "${LOCAL_SSH_KEY}" ] && [ -n "${LOCAL_SSH_PORT}" ] && [ -n "${LOCAL_SSH_USER}" ] && [ -n "${LOCAL_SSH_HOST}" ] && [ -n "${LOCAL_DB_PATH}" ]; then
-		echo "umount /mnt/S2" >> /cron-script
-	fi
+if [ -n "${S2_SSH_KEY}" ] && [ -n "${S2_SSH_PORT}" ] && [ -n "${S2_SSH_USER}" ] && [ -n "${S2_SSH_HOST}" ] && [ -n "${S2_SSH_PATH}" ]; then
+	echo "umount /mnt/S2" >> /cron-script
 fi
-
 chmod +x /cron-script
 
 if [ "${INITIALRUN}" == "true" ]; then
